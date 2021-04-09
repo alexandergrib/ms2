874fa3ecd9056e452d15e93a2fdd1083a64b1422
Alex <pioneerko@hotmail.com>
2021-04-09 15:27:40 +0100

    Added weather api calls

---
+++ assets/js/1.js
@@ -0,0 +1,62 @@
+// weather api code main logic from
+// https://webdesign.tutsplus.com/tutorials/build-a-simple-weather-app-with-vanilla-javascript--cms-33893
+// part of this code modified by me.
+const form = document.querySelector(".search-div form");
+const input = document.querySelector("#searchField");
+const msg = document.querySelector(".search-div .msg");
+const list = document.querySelector(".ajax-section .cities");
+
+const apiKey = "888a73a8b27eb54c8d1ba56ed02ec435";
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const inputVal = input.value;
+
+    //ajax here
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const {coord, main, name, sys, weather} = data;
+            const icon = `https://openweathermap.org/img/wn/${
+                weather[0]["icon"]
+            }@2x.png`;
+            document.getElementById("info-text").classList.add("hide");  //hides instructions container
+            document.getElementById("main").classList.remove("hide");  // show all data container
+            //checks status fo checkbox
+            let checkbox = document.querySelector("input[name=checkbox]");
+            if (checkbox.checked) {
+                console.log("Checkbox is checked..");
+            } else {
+                console.log("Checkbox is not checked..");
+            }
+            //TODO add checkbox save its status on local storage when update page it keeps its selection
+
+            const li = document.createElement("li");
+            li.classList.add("city");
+            const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+            li.innerHTML = markup;
+            list.appendChild(li);
+            document.getElementById("map").innerHTML = `${name}, ${coord.lat}, ${coord.lon} `;  //replace with call to google map api
+
+        })
+        .catch(() => {
+            msg.textContent = "Please search for a valid city 😩";
+        });
+
+    msg.textContent = "";
+    form.reset();
+    input.focus();
+});
+
