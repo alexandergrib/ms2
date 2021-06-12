12ac768228f7b8c01a8303330ad8c8470db37579
Alex <pioneerko@hotmail.com>
2021-06-12 19:27:59 +0100

    Connected covid API to display data in covid info section

--- assets/js/covid.js
+++ assets/js/covid.js
@@ -1,7 +1,6 @@
 //api from here https://documenter.getpostman.com/view/10808728/SzS8rjbc#00030720-fae3-4c72-8aea-ad01ba17adf8
 
 
-// const country = 'RU'.toUpperCase()
 //function called from weather.js L111
 function getCovidInfo(country) {
     const url = `https://api.covid19api.com/summary`;
@@ -15,27 +14,46 @@ function covidHandler(data, country) {
     const {ID, Message, Global, Countries} = data;
     // console.log('data', data);
 
-
+    const globalNewCases = document.getElementById("global_new_cases");
+    const globalNewDeath = document.getElementById("global_new_death");
+    const globalDeath = document.getElementById("global_death");
+    globalNewCases.innerHTML = Global.NewConfirmed;
+    globalNewDeath.innerHTML = Global.NewDeaths;
+    globalDeath.innerHTML = Global.TotalDeaths;
     //display global data
-    //  console.log(Global);
-    console.log('NewConfirmed', Global.NewConfirmed)
-    console.log('NewDeaths', Global.NewDeaths)
-    console.log('TotalDeaths', Global.TotalDeaths)
-    console.log('NewRecovered', Global.NewRecovered)
-    console.log('TotalRecovered', Global.TotalRecovered)
+    // console.log(Global);
+    // console.log('NewConfirmed', Global.NewConfirmed);
+    // console.log('NewDeaths', Global.NewDeaths);
+    // console.log('TotalDeaths', Global.TotalDeaths);
+    // console.log('NewRecovered', Global.NewRecovered);
+    // console.log('TotalRecovered', Global.TotalRecovered);
     //display by country
     // console.log(Countries);
     Countries.forEach(element => getEachCountry(element, country));
 }
 
-function getEachCountry(e, country){
+function getEachCountry(e, country) {
+    const localCountryHdr = document.getElementById("covid_local_hdr")
+    const localNewCases = document.getElementById("local_new_cases");
+    const localNewDeath = document.getElementById("local_new_death");
+    const localDeath = document.getElementById("local_death");
     if (e.CountryCode === country) {
-        console.log(e);
+        localNewCases.innerHTML = e.NewConfirmed;
+        localNewDeath.innerHTML = e.NewDeaths;
+        localDeath.innerHTML = e.TotalDeaths;
+        localCountryHdr.innerHTML = `COVID-19 in ${e.Country}`;
+        // console.log(e);
     }
 
 }
 
-
+//covid_local_hdr   COVID-19 GB stats
+// global_new_cases
+// global_new_death
+// global_death
+// local_new_cases
+// local_new_death
+// local_death
 
 
 // getCovidInfo(country)
